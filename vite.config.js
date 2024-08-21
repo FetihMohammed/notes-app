d695670346ce2661dc027a6c6fb220a186c4b56f
Fetih <yearly990@gmail.com>
2024-08-21 12:54:52 +0300

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'repository-name' with the actual name of your repository
-const base = '/notes-app'
 
 export default defineConfig({
   plugins: [react()],
-  base: base,
   build: {
     outDir: 'dist', // Default output directory, you can change if needed
   }
