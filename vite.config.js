adabc229c42c1ccb9a8ff1aa0e06d2037a59e241
Fetih <yearly990@gmail.com>
2024-08-21 13:05:32 +0300

    dne

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Default output directory, you can change if needed
-  }
-})
+  base: "/notes-app/",
+});
