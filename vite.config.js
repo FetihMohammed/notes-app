816c370e72a8ca8665d225126c5a25399fa4991d
Fetih <yearly990@gmail.com>
2024-08-21 12:22:35 +0300

    deployment configured

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Replace 'repository-name' with the actual name of your repository
+const base = 'https://github.com/FetihMohammed/notes-app/'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: base,
+  build: {
+    outDir: 'dist', // Default output directory, you can change if needed
+  }
 })
